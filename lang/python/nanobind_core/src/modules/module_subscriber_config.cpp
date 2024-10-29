ee5f4333da188242fec6ff49026005ef3d6dec61
Arifulla Shariff <166038015+ashariff-11@users.noreply.github.com>
2024-10-29 13:52:00 +0100

    subscriber.h and publisher.h nanobind files

--- lang/python/nanobind_core/src/modules/module_subscriber_config.cpp
+++ lang/python/nanobind_core/src/modules/module_subscriber_config.cpp
@@ -18,30 +18,47 @@
 */
 
 /**
- * @brief  Add ServiceClient class to nanobind module
+ * @brief  Add Subscriber class to nanobind module
 **/
 
 
 #include <modules/module_subscriber_config.h>
-#include <wrappers/wrapper_subscriber_config.h>
+#include <ecal/config/subscriber.h>
+
+namespace nb = nanobind;
 
 void AddSubscriberConfigStructToModule(nanobind::module_& module)
 {
-    nanobind::class_<eCAL::Subscriber::SHM::CNBSHMConfiguration>(module, "SHMConfiguration")
-        .def(nanobind::init<>())
-        .def_rw("enable", &eCAL::Subscriber::SHM::CNBSHMConfiguration::enable);
+    nb::module_ m_eCAL = module.def_submodule("eCAL");
+    nb::module_ m_Subscriber = m_eCAL.def_submodule("Subscriber");
+    nb::module_ m_Layer = m_Subscriber.def_submodule("Layer");
 
-    nanobind::class_<eCAL::Subscriber::UDP::CNBUDPConfiguration>(module, "UDPConfiguration")
-        .def(nanobind::init<>())
-        .def_rw("enable", &eCAL::Subscriber::UDP::CNBUDPConfiguration::enable);
+    // Binding SHM Layer Configuration
+    nb::class_<eCAL::Subscriber::Layer::SHM::Configuration>(m_Layer, "SHM_Configuration")
+        .def(nb::init<>())
+        .def_rw("enable", &eCAL::Subscriber::Layer::SHM::Configuration::enable);
 
-    nanobind::class_<eCAL::Subscriber::TCP::CNBTCPConfiguration>(module, "TCPConfiguration")
-        .def(nanobind::init<>())
-        .def_rw("enable", &eCAL::Subscriber::TCP::CNBTCPConfiguration::enable);
+    // Binding UDP Layer Configuration
+    nb::class_<eCAL::Subscriber::Layer::UDP::Configuration>(m_Layer, "UDP_Configuration")
+        .def(nb::init<>())
+        .def_rw("enable", &eCAL::Subscriber::Layer::UDP::Configuration::enable);
 
-    nanobind::class_<eCAL::Subscriber::CNBSubscriberConfiguration>(module, "SubscriberConfiguration")
-        .def(nanobind::init<>())
-        .def_rw("shm", &eCAL::Subscriber::CNBSubscriberConfiguration::shm)
-        .def_rw("udp", &eCAL::Subscriber::CNBSubscriberConfiguration::udp)
-        .def_rw("tcp", &eCAL::Subscriber::CNBSubscriberConfiguration::tcp);
+    // Binding TCP Layer Configuration
+    nb::class_<eCAL::Subscriber::Layer::TCP::Configuration>(m_Layer, "TCP_Configuration")
+        .def(nb::init<>())
+        .def_rw("enable", &eCAL::Subscriber::Layer::TCP::Configuration::enable);
+
+    // Binding Layer Configuration
+    nb::class_<eCAL::Subscriber::Layer::Configuration>(m_Layer, "LayerConfiguration")
+        .def(nb::init<>())
+        .def_rw("shm", &eCAL::Subscriber::Layer::Configuration::shm)
+        .def_rw("udp", &eCAL::Subscriber::Layer::Configuration::udp)
+        .def_rw("tcp", &eCAL::Subscriber::Layer::Configuration::tcp);
+
+    // Binding Subscriber Configuration
+    nb::class_<eCAL::Subscriber::Configuration>(m_Subscriber, "SubscriberConfiguration")
+        .def(nb::init<>())
+        .def_rw("layer", &eCAL::Subscriber::Configuration::layer)
+        .def_rw("drop_out_of_order_messages", &eCAL::Subscriber::Configuration::drop_out_of_order_messages);
+
 }
